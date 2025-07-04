4a401c9b20102ea5eb0f2abc8dd573a8828980e8
Marios Grivas <148717455+Unf0undedOmn1s@users.noreply.github.com>
2025-07-04 14:47:16 +0300

    Add files via upload

---
+++ pdf-analysis/analyzer.py
@@ -0,0 +1,9 @@
+from modules.banner import print_banner
+from modules.menu import show_menu
+
+def main():
+    print_banner()
+    show_menu()
+
+if __name__ == "__main__":
+    main()
---
+++ pdf-analysis/utils/metadata_extractor.py
@@ -0,0 +1,11 @@
+from PyPDF2 import PdfReader
+
+def extract_metadata(file_path):
+    try:
+        reader = PdfReader(file_path)
+        metadata = reader.metadata
+        print("\n[+] PDF Metadata:")
+        for key, value in metadata.items():
+            print(f"  {key}: {value}")
+    except Exception as e:
+        print(f"[-] Error reading PDF metadata: {e}")
